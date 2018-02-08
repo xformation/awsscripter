20f86d198669d4bcb70aeaad5e518d0f8e956132
papu <papu.bhattacharya@synectiks.com>
2018-02-08 13:49:20 +0530

    added CISP audit command implementation

--- awsscripter/cli/audit/cispaudit.py
+++ awsscripter/cli/audit/cispaudit.py
@@ -1,7 +1,7 @@
 import click
 
 from awsscripter.stack.helpers import catch_exceptions, confirmation
-
+from awsscripter.audit.Auditor import Auditor
 @click.command(name="CISP")
 @click.argument("level")
 @click.pass_context
@@ -11,3 +11,6 @@ def audit_cisp_command(ctx, level):
     Depending on level , it will perform the CISP audit, by default the level is FULL
     """
     print("Auditing with level  " + level)
+    auditor = Auditor("myname", "myproject", "us-east-1")
+    auditor.handle("test", "test")
+
