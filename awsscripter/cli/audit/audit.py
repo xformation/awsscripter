76a44824000ab9b48ebc0f7e3e2f68a9e413e371
papu <papu.bhattacharya@synectiks.com>
2018-02-08 15:13:45 +0530

    added CISP audit

--- awsscripter/cli/audit/audit.py
+++ awsscripter/cli/audit/audit.py
@@ -2,9 +2,9 @@ import click
 
 from awsscripter.cli.audit.cispaudit import audit_cisp_command
 from awsscripter.cli.audit.hippaaudit import audit_hippa_command
-
-@click.group(name="audit")
-def audit_group():
+@click.group(name="audit", chain=True)
+@click.pass_context
+def audit_group(ctx):
     """
     Commands for auditing aws environment with awsscripter. This will iclude CISP/ HIPPA Audit for now.
 
--- awsscripter/cli/audit/cispaudit.py
+++ awsscripter/cli/audit/cispaudit.py
@@ -1,16 +1,19 @@
 import click
-
 from awsscripter.stack.helpers import catch_exceptions, confirmation
 from awsscripter.audit.Auditor import Auditor
+import logging
 @click.command(name="CISP")
-@click.argument("level")
+@click.option(
+    "--level", type=click.Choice(["FULL", "LESS"]), default="FULL",
+    help="The level of audit , default is full")
 @click.pass_context
 @catch_exceptions
 def audit_cisp_command(ctx, level):
     """
     Depending on level , it will perform the CISP audit, by default the level is FULL
     """
-    print("Auditing with level  " + level)
+    logger = logging.getLogger(__name__)
+    logger.info("Auditing with level  " + level)
     auditor = Auditor("myname", "myproject", "us-east-1")
     auditor.handle("test", "test")
 
