af948e6ed2e629b7612adc02b929401fd8165e3f
Balaram <balaramkolluru@gmail.com>
2018-04-06 12:11:13 +0530

    cispaudit
    cispaudit

--- awsscripter/cli/audit/cispaudit.py
+++ awsscripter/cli/audit/cispaudit.py
@@ -15,5 +15,5 @@ def audit_cisp_command(ctx, level):
     logger = logging.getLogger(__name__)
     logger.info("Auditing with level  " + level)
     auditor = Auditor("myname", "myproject", "us-east-1")
-    auditor.handle("test", "test")
+    auditor.ha("test", "test")
 
