b36eb5199e0cbc7a4c939eaa2efb802e08d2643b
shahbaz Alam <shabaz.alam@synectiks.com>
2018-06-25 11:52:25 +0530

    Dynamic path and index name
    Dynamic path ,ES IP,port and Index name can be passed using cmd.

--- awsscripter/billing/hourlybilltoes.py
+++ awsscripter/billing/hourlybilltoes.py
@@ -9,20 +9,10 @@ import os
 import sys
 import urllib.request
 from urllib.request import Request, urlopen  # Python 3
+from awsscripter.common.LambdaBase import LambdaBase
+
 
 class Billtoes():
-
-    def __init__(self,path=None,read_es_security_enable = False,write_es_security_enable = False):
-        self.read_es_security_enable=False
-        self.write_es_security_enable=False
-        self.path=path
-    elasticServer = os.environ.get('ES_CPU_CLUSTER_URL', 'http://10.10.10.50:4571')
-    interval = int(os.environ.get('ES_CPU_INTERVAL', '60'))
-
-    # ElasticSearch Cluster to Send Metrics
-    elasticIndex = os.environ.get('ES_METRICS_INDEX_NAME', 'scripter_report')
-    elasticMonitoringCluster = os.environ.get('ES_METRICS_CPUMONITORING_CLUSTER_URL', 'http://10.10.10.50:4571')
-
     # Enable Elasticsearch Security
     # read_username and read_password for read ES cluster information
     # write_username and write_passowrd for write monitor metric to ES.
@@ -33,6 +23,30 @@ class Billtoes():
     write_es_security_enable = False
     write_username = "write_username"
     write_password = "write_password"
+
+    # elasticServer = ""
+    # interval = ""
+    # elasticIndex = ""
+    # elasticMonitoringCluster = ""
+
+    def __init__(self,path,esurl,index,read_es_security_enable = False,write_es_security_enable = False):
+        self.read_es_security_enable=False
+        self.write_es_security_enable=False
+        self.path=path
+        self.esurl=esurl
+        self.index=index
+        self.elasticServer = os.environ.get('ES_CPU_CLUSTER_URL', esurl)
+        self.elasticIndex = os.environ.get('ES_METRICS_INDEX_NAME', index)
+        self.elasticMonitoringCluster = os.environ.get('ES_METRICS_CPUMONITORING_CLUSTER_URL', esurl)
+
+    # urlvar = 'http://10.10.10.50:4571'
+    # elasticServer = os.environ.get('ES_CPU_CLUSTER_URL', urlvar)
+    # interval = int(os.environ.get('ES_CPU_INTERVAL', '60'))
+    #
+    # # ElasticSearch Cluster to Send Metrics
+    # elasticIndex = os.environ.get('ES_METRICS_INDEX_NAME', 'scripter_report')
+    # elasticMonitoringCluster = os.environ.get('ES_METRICS_CPUMONITORING_CLUSTER_URL', 'http://10.10.10.50:4571')
+
 
     def handle_urlopen(self,urlData, username, password):
         if self.read_es_security_enable:
@@ -54,21 +68,22 @@ class Billtoes():
             return value
 
 
-    def fetch_clusterhealth(self,path):
+    def fetch_clusterhealth(self):
         try:
             jsonData = {}
             utc_datetime = datetime.datetime.utcnow()
             endpoint = "/_cluster/health"
             urlData = self.elasticServer #+ endpoint
+            print(urlData)
             response = self.handle_urlopen(urlData,username=None, password=None)
             # with open("D:\\Hourlybilling_report-1.csv") as csvfile:
-            with open(path) as csvfile:
+            with open(self.path) as csvfile:
                 reader = csv.DictReader(csvfile)
                 title = reader.fieldnames
                 for row in reader:
                     for key, val in row.items():
                         jsonData[key] = self.myconverter(val)
-                    print(jsonData)
+                    # print(jsonData)
                     self.post_data(jsonData, username=None, password=None)
             clusterName = 'elasticsearch'
             return clusterName
@@ -106,5 +121,19 @@ class Billtoes():
         except Exception as e:
             print ("Error:  {}".format(str(e)))
 
-    def starter(self,path):
-        clusterName = self.fetch_clusterhealth(path)
+    def starter(self):
+        # urlvar = 'http://10.10.10.50:4571'
+        print(self.esurl)
+        elasticServer = os.environ.get('ES_CPU_CLUSTER_URL', self.esurl)
+        print(elasticServer)
+        interval = int(os.environ.get('ES_CPU_INTERVAL', '60'))
+
+        # ElasticSearch Cluster to Send Metrics
+        elasticIndex = os.environ.get('ES_METRICS_INDEX_NAME', self.index)
+        elasticMonitoringCluster = os.environ.get('ES_METRICS_CPUMONITORING_CLUSTER_URL', 'http://10.10.10.50:4571')
+        clusterName = self.fetch_clusterhealth()
+# if __name__ == '__main__':
+#     path="D:\\Hourlybilling_report-1.csv"
+#     esurl='http://10.10.10.50:4571'
+#     biller = Billtoes(path, esurl)
+#     biller.starter()
--- awsscripter/cli/awsbilling/hbrtoes.py
+++ awsscripter/cli/awsbilling/hbrtoes.py
@@ -8,13 +8,18 @@ import logging
     "--level", type=click.Choice(["FULL", "LESS"]), default="FULL",
     help="The level of audit , default is full")
 @click.option("--path",prompt="Enter report name with absolute path",help="Enter file path")
+@click.option("--hostip",prompt="ES IP")
+@click.option("--port",prompt="ES port number")
+@click.option("--index",prompt="Index name")
 @click.pass_context
 @catch_exceptions
-def hbrtoes_command(ctx, level,path):
+def hbrtoes_command(ctx, level,path,hostip,port,index):
     """
     Depending on level , it will perform the Billing audit, by default the level is FULL
     """
     logger = logging.getLogger(__name__)
     logger.info("Hourly Billing Report" + level)
-    biller = Billtoes()
-    biller.starter(path)
+    esurl=str("http://"+str(hostip)+":"+str(port))
+    print(esurl)
+    biller = Billtoes(path,esurl,index)
+    biller.starter()
